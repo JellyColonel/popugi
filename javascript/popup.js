86788a54b66dfe4b519aff8c5e4ae46f9c592e70
Valeriu Gritco <valeriu.gritco.md@gmail.com>
2022-06-10 18:30:36 +0300

    working pop up on hover

--- javascript/popup.js
+++ javascript/popup.js
@@ -1,5 +1,5 @@
-$('#enter img').hover(function(){
+$('img').hover(function(){
     this.src = "style/image/TestImage.png";
 },function(){
-    this.src = "style/image/banner.png";
+    this.src = "style/image/feeding/feeding_2.jpg";
 });
